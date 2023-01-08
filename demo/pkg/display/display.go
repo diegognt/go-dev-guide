294349cde4d16c7b6ea6b0918b390eaadedc7358
Diego Navarro <diego.nava07@gmail.com>
2023-01-08 13:51:01 -0500

    Packages lesson
    This PR includes:
      - The code for the "Packages" lesson demo

---
+++ demo/pkg/display/display.go
@@ -0,0 +1,10 @@
+// --Summary:
+// Simple package to display a message in the terminal.
+
+package display
+
+import "fmt"
+
+func Display(message string) {
+  fmt.Println(message)
+}
---
+++ demo/pkg/message/emote.go
@@ -0,0 +1,10 @@
+// Summary:
+// Shows a message in the termimal using emotions.
+
+package message
+
+import "fmt"
+
+func Exciting(message string) {
+  fmt.Printf("%v!\n", message)
+}
